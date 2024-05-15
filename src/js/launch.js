8e7b61ee53acb715b4c316064abcca16b6e6fab8
826m <Kevin.Prince@palmettogbaservices.com>
2024-05-14 21:01:46 -0400

    Swapped to EoB

--- src/js/launch.js
+++ src/js/launch.js
@@ -1,5 +1,5 @@
 FHIR.oauth2.authorize({
     "client_id": "test_smart_app",
-    "scope": "launch/patient openid profile patient/Patient.read patient/ExplanationOfBenefit.read",
+    "scope": "launch/patient openid profile patient/*.read",
     "redirect_uri": "demo.html"
 });
